8382134259863acce5507f759614ed1871378efa
Sebrekov <admin@ittrue.ru>
2023-02-26 11:43:41 +0300

    Make 1 game

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
-const getName = (name) => {
-name = readlineSync.question('May I have your name? ');//Get name user
-return console.log(`Hello, ${name}!`);
+
+const getName = () => {
+  const nameUser = readlineSync.question('May I have your name? '); // Get name user
+  return console.log(`Hello, ${nameUser}!`);
 };
 export default getName;
-
