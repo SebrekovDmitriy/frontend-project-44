cc1eb9f5697a44c1f1a31d25e6633424682f6d27
Sebrekov <admin@ittrue.ru>
2023-05-26 03:19:13 +0300

    Make 5 game part 2

--- src/prime.js
+++ src/prime.js
@@ -2,13 +2,13 @@ import roundGames from "../src/index.js";
 import getRandom from "../src/getRandom.js";
 
 const prime = () => {
-  let answerCorrect = "";
   let resultPrime = [
     'Answer "yes" if given number is prime. Otherwise answer "no".',
   ];
   for (let i = 0; i < 3; i++) {
+    let answerCorrect = "yes";
     let questionPrime = "";
-    const numberPrime = 57;//getRandom(1, 100); // setting the range of random number 1
+    const numberPrime = getRandom(2, 101); // setting the range of random number 1
     questionPrime = numberPrime;
     for (
       var startNumber = 2;
@@ -16,15 +16,12 @@ const prime = () => {
       ++startNumber
     ) {
       if (numberPrime % startNumber === 0) {
-        answerCorrect = "no"; // check the remainder of the division by 2
-      } else {
-        answerCorrect = "yes";
+        answerCorrect = "no";
       }
     }
     resultPrime.push([questionPrime, answerCorrect]);
-    
   }
-  console.log(resultPrime);
+
   return resultPrime;
 };
 export default prime;
